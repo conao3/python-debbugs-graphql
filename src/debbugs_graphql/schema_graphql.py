854acbc7d7919bdc59a9ceba028901a817c1bf6a
Naoya Yamashita <conao3@gmail.com>
2022-09-27 15:11:49 +0900

    use classmethod instead of global function

--- src/debbugs_graphql/schema_graphql.py
+++ src/debbugs_graphql/schema_graphql.py
@@ -1,24 +1,24 @@
 import strawberry
 
 
-def get_books():
-    return [
-        Book(
-            title='The Great Gatsby',
-            author='F. Scott Fitzgerald',
-        ),
-    ]
-
-
 @strawberry.type
 class Book:
     title: str
     author: str
 
+    @classmethod
+    def get_books(cls):
+        return [
+            Book(
+                title='The Great Gatsby',
+                author='F. Scott Fitzgerald',
+            ),
+        ]
+
 
 @strawberry.type
 class Query:
-    books: list[Book] = strawberry.field(resolver=get_books)
+    books: list[Book] = strawberry.field(resolver=Book.get_books)
 
 
 schema = strawberry.Schema(query=Query)
