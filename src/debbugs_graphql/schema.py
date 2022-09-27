374bc9935f5ff6fec3cfb8dbad9ec13b780a49e5
Naoya Yamashita <conao3@gmail.com>
2022-09-27 13:53:50 +0900

    add strawberry first schema

---
+++ src/debbugs_graphql/schema.py
@@ -0,0 +1,24 @@
+import strawberry
+
+
+def get_books():
+    return [
+        Book(
+            title='The Great Gatsby',
+            author='F. Scott Fitzgerald',
+        ),
+    ]
+
+
+@strawberry.type
+class Book:
+    title: str
+    author: str
+
+
+@strawberry.type
+class Query:
+    books: list[Book] = strawberry.field(resolver=get_books)
+
+
+schema = strawberry.Schema(query=Query)
