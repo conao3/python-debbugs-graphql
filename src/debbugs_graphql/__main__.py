eb542d35bcbc231ebfe001e1accc929c99792059
Naoya Yamashita <conao3@gmail.com>
2022-09-27 14:39:11 +0900

    pass uvicorn argument

--- src/debbugs_graphql/__main__.py
+++ src/debbugs_graphql/__main__.py
@@ -1,8 +1,25 @@
+import argparse
 import uvicorn
 
 
 def main():
-    uvicorn.run("debbugs_graphql.main:app")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--port', default=8000, type=int)
+    parser.add_argument('--reload', action='store_true')
+    args = parser.parse_args()
+
+    uvicorn.run(
+        "debbugs_graphql.main:app",
+        **{
+            key: getattr(args, key)
+            for key in (
+                'host',
+                'port',
+                'reload',
+            )
+        }
+    )
 
 
 if __name__ == '__main__':
